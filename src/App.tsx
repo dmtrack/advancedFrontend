a830392623216df62fd5020dee52fc9e9794a5ff
dmtrack <dmtrack.dev@gmail.com>
2023-09-26 18:34:32 +0500

    feature: classnames and theme context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import React, { Suspense } from 'react';
+import './styles/index.scss';
+import { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
-
-import './index.scss';
 import { AboutPageAsync } from './pages/AboutPage/About-page.async';
 import { MainPageAsync } from './pages/MainPage/Main-page.async';
+import { useTheme } from './theme/useTheme';
+import { classNames } from './helpers/classNames/classNames';
 
 const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div className='app'>
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>TOGGLE THEME</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
